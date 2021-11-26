2917fadb873f7cce9079776dddf5f1b43bfd39d3
gustavo <noticiasgus@gmail.com>
2021-11-26 08:47:39 +0100

    cambios

---
+++ static/js/myjs.js
@@ -0,0 +1,23 @@
+
+ $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+    margin:2,
+    autoWidth: true,
+    dots: false,
+    nav: true,
+    navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+    responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+  })
+
+});
+   
